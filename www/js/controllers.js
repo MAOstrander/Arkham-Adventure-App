04c3f033d2e2bfd544624842f65d13e9b177b70f
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-01-18 09:48:26 -0600

    added ids and names to all items in current expansion, load the data in from the json file instead of hardcoded

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,44 +15,49 @@ angular.module('starter.controllers', [])
     testData.success(function(res) {
 
       console.log("testData", res);
-      var investigators = res.investigators;
-      mainMenu.testTheFactory = [];
-      for (var thing in investigators) {
-        console.log("What is this?", thing);
-        mainMenu.testTheFactory.push(thing);
-      }
+
+      mainMenu.allCharacters = res.investigators;
+      mainMenu.allAncient = res['ancient ones'];
+      console.log("mainMenu.allCharacters", mainMenu.allCharacters);
+      console.log("mainMenu.allAncient", mainMenu.allAncient);
+      //  investigators = res.investigators;
+      // mainMenu.testTheFactory = [];
+      // for (var thing in investigators) {
+      //   console.log("What is this?", thing);
+      //   mainMenu.testTheFactory.push(thing);
+      // }
       // var amandaHome = investigators['Amanda Sharpe'].home;
     // alert(mainMenu.testTheFactory);
     })
 
-  mainMenu.allCharacters = [
-    { title: 'Amanda Sharpe', id: 0 },
-    { title: 'Ashcane Pete', id: 1 },
-    { title: 'Bob Jenkins', id: 2 },
-    { title: 'Carolyn Fern', id: 3 },
-    { title: 'Darrell Simmons', id: 4 },
-    { title: 'Dexter Drake', id: 5 },
-    { title: 'Gloria Goldberg', id: 6 },
-    { title: 'Harvey Walters', id: 7 },
-    { title: 'Jenny Barnes', id: 8 },
-    { title: 'Joe Diamond', id: 9 },
-    { title: 'Kate Winthrop', id: 10 },
-    { title: 'Mandy Thompson', id: 11 },
-    { title: 'Michael McGlen', id: 12 },
-    { title: 'Monterey Jack', id: 13 },
-    { title: 'Sister Mary', id: 14 },
-    { title: 'Vincent Lee', id: 15 }
-  ];
-  mainMenu.allAncient = [
-    { title: 'Azathoth', id: 0 },
-    { title: 'Cthulu', id: 1 },
-    { title: 'Hastur', id: 2 },
-    { title: 'Ithaqua', id: 3 },
-    { title: 'Nyarlathotep', id: 4 },
-    { title: 'Shub-Niggurath', id: 5 },
-    { title: 'Yig', id: 6 },
-    { title: 'Yog-Sothoth', id: 7 }
-  ];
+  // mainMenu.allCharacters = [
+  //   { title: 'Amanda Sharpe', id: 0 },
+  //   { title: 'Ashcane Pete', id: 1 },
+  //   { title: 'Bob Jenkins', id: 2 },
+  //   { title: 'Carolyn Fern', id: 3 },
+  //   { title: 'Darrell Simmons', id: 4 },
+  //   { title: 'Dexter Drake', id: 5 },
+  //   { title: 'Gloria Goldberg', id: 6 },
+  //   { title: 'Harvey Walters', id: 7 },
+  //   { title: 'Jenny Barnes', id: 8 },
+  //   { title: 'Joe Diamond', id: 9 },
+  //   { title: 'Kate Winthrop', id: 10 },
+  //   { title: 'Mandy Thompson', id: 11 },
+  //   { title: 'Michael McGlen', id: 12 },
+  //   { title: 'Monterey Jack', id: 13 },
+  //   { title: 'Sister Mary', id: 14 },
+  //   { title: 'Vincent Lee', id: 15 }
+  // ];
+  // mainMenu.allAncient = [
+  //   { title: 'Azathoth', id: 0 },
+  //   { title: 'Cthulu', id: 1 },
+  //   { title: 'Hastur', id: 2 },
+  //   { title: 'Ithaqua', id: 3 },
+  //   { title: 'Nyarlathotep', id: 4 },
+  //   { title: 'Shub-Niggurath', id: 5 },
+  //   { title: 'Yig', id: 6 },
+  //   { title: 'Yog-Sothoth', id: 7 }
+  // ];
   mainMenu.allHerald = [
     { title: 'Father Dagon', id: 0 },
     { title: 'Mother Hydra', id: 1 },
@@ -68,7 +73,7 @@ angular.module('starter.controllers', [])
     random = Math.floor(Math.random()* checkedArray.length);
 
     // Store which investigator that is
-    // chosenPlayers[i] = checkedArray[random].title;
+    // chosenPlayers[i] = checkedArray[random].name;
     chosenPlayers[i] = checkedArray[random];
 
     //Use splice to remove chosen player from selected array for next player
@@ -156,5 +161,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('CharCtrl', function($scope, $stateParams) {
+  console.log("WHAT DOES IT LOOK LIKE WHEN I ARRIVE", $stateParams);
+  console.log("mainMenu.allCharacters", $scope.$parent.mainMenu.allCharacters);
    $scope.where = $stateParams.id;
 });
