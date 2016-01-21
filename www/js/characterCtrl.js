7bfc1538b1048bc791f48c3ade8ffc5c78a5c62a
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-01-20 20:01:48 -0600

    pulled the character controller out into a seperate file

---
+++ www/js/characterCtrl.js
@@ -0,0 +1,26 @@
+angular.module('starter.characterCtrl', [])
+
+.controller('CharCtrl', function($scope, $stateParams, mainInfoFactory) {
+  var chara = this;
+
+  var newCall = mainInfoFactory.getMainInfo();
+  newCall.success(function(res) {
+
+    console.log("newCall", res);
+
+    for (var key in res.investigators) {
+     if (res.investigators.hasOwnProperty(key)) {
+      var obj = res.investigators[key];
+       if (obj.id === parseInt($stateParams.id)) {
+          chara.loaded = obj;
+          console.log("HEY!");
+        }
+      }
+    }
+
+    console.log("chara.loaded", chara.loaded);
+    console.log("$stateParams", $stateParams);
+
+    // $scope.$apply();
+  })
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -133,27 +133,4 @@ angular.module('starter.controllers', [])
 
 .controller('ResearchCtrl', function($scope) {
 
-})
-
-.controller('CharCtrl', function($scope, $stateParams, mainInfoFactory) {
-  var chara = this;
-
-  var newCall = mainInfoFactory.getMainInfo();
-  newCall.success(function(res) {
-
-    console.log("newCall", res);
-
-    for (var key in res.investigators) {
-     if (res.investigators.hasOwnProperty(key)) {
-      var obj = res.investigators[key];
-       if (obj.id === parseInt($stateParams.id)) {
-          chara.loaded = obj;
-          console.log("HEY!");
-        }
-      }
-    }
-
-    console.log("chara.loaded", chara.loaded);
-    console.log("$stateParams", $stateParams);
-  })
 });
